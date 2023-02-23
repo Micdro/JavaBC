30910d40e6246500596c3cbec8eb3a2fafde9eb9
micdro <sehser5@hotmail.com>
2023-02-22 22:39:04 -0500

    Created a Car class which have getter and setter methods to be used in my main class.

---
+++ src/Car.java
@@ -0,0 +1,63 @@
+/**
+ * This is the Car class which will return the current vehicle type.
+ */
+public class Car {
+
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String color = "White";
+    private int doors = 4;
+    private boolean convertible = true;
+
+    public void describeCar()
+    {
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : "")
+        );
+    }
+
+    public void setMake(String make)
+    {
+        this.make = make;
+    }
+
+    public String getMake()
+    {
+        return this.make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setMake("Hello");
+        car.describeCar();
         System.out.println("Hello world!");
     }
 }
