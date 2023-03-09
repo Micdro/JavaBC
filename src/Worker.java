3cf31c0972a9bc46068b95d63b8d289b0a89c64f
Michael Drougas <sehser5@hotmail.com>
2023-03-09 16:57:52 -0500

    Completed inheritance challenge.

---
+++ src/Worker.java
@@ -0,0 +1,91 @@
+import java.time.LocalDate;
+
+public class Worker {
+    private String name;
+    private String birthDate;
+    private String endDate;
+
+    public Worker(String name, String birthDate){
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public static void main(String[] args) {
+        Employee michael = new Employee("Michael", "03/22/1989",  "01/01/2021");
+        System.out.println(michael);
+        System.out.println("Age = " + michael.getAge());
+        System.out.println("Pay = " + michael.collectPay());
+
+        Employee alice = new Employee("Alice", "04/23/2020", "01/01/2022");
+        System.out.println(alice);
+    }
+    public int getAge(){
+        int currentYear =  LocalDate.now().getYear();
+        int birthYear = Integer.parseInt(birthDate.substring(6));
+        return (currentYear - birthYear);
+    }
+    public double collectPay(){
+        return 0.0;
+    }
+
+    public void terminate(String endDate){
+    this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+}
+
+class Employee extends Worker {
+    private long employeeId;
+    private String hireDate;
+    private static int employeeNumber = 1;
+    public Employee(String name, String birthDate, String hireDate) {
+        super(name, birthDate);
+        this.employeeId = Employee.employeeNumber++;
+        this.hireDate = hireDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", hireDate='" + hireDate + '\'' +
+                "} " + super.toString();
+    }
+}
+
+class SalariedEmployee extends Employee{
+    private double annualSalary;
+    private boolean isRetired;
+
+    public SalariedEmployee(String name, String birthDate, long employeeId, String hireDate, double annualSalary, boolean isRetired) {
+        super(name, birthDate,  hireDate);
+        this.annualSalary = annualSalary;
+        this.isRetired = isRetired;
+    }
+
+
+    private void retire(){
+
+    }
+}
+
+class HourlyEmployee extends Employee{
+    private double hourlyPayRate;
+
+    public HourlyEmployee(String name, String birthDate, long employeeId, String hireDate, double hourlyPayRate) {
+        super(name, birthDate, hireDate);
+        this.hourlyPayRate = hourlyPayRate;
+    }
+
+    private void getDoublePay(){
+
+    }
+}
