9a70612035988ac4da791ef3836b6a261ef7b192
Michael Drougas <sehser5@hotmail.com>
2023-03-16 13:56:33 -0400

    Created a class to determine the volume of a Cuboid pool

---
+++ src/Rectangle.java
@@ -0,0 +1,25 @@
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if (width < 0) width = 0;
+        if (length < 0) length = 0;
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        if (width < 0) width = 0;
+        return width;
+    }
+
+    public double getLength() {
+        if (length < 0) length = 0;
+        return length;
+    }
+
+    public double getArea() {
+        return (width * length);
+    }
+}
